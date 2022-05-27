1781c3beaa5acbdf1492791fc1d0549b9c175b43
jyablonski9 <jyablonski9@gmail.com>
2022-05-27 14:15:16 -0500

    add favicon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import typing
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 import strawberry
 from strawberry.asgi import GraphQL
 
@@ -11,8 +12,12 @@ graphql_app = GraphQL(schema)
 app = FastAPI()
 app.add_route("/graphql", graphql_app)
 app.add_websocket_route("/graphql", graphql_app)
-
+app.mount("/static", StaticFiles(directory="static"), name="favicon.ico")
 
 @app.get("/")
 async def root():
     return RedirectResponse("/graphql")
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('static/favicon.ico')
